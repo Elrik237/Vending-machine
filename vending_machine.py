5657d06515dd885c1dfabf5fd872e21897351eb5
Эдуард Поляков <76116843+Elrik237@users.noreply.github.com>
2021-02-01 13:22:04 +0300

    Реализован интерфейс менеджера

--- vending_machine.py
+++ vending_machine.py
@@ -1,37 +1,47 @@
-from model_snacks import ListOfSnacks
+import os.path
 from parsing import Parser
-
 from storage_service import StorageService
+from model_snacks import ListOfSnacks
 
 
 class VendingMachine:
-    def __init__(self):
+    def __init__(self, start=None):
 
         self.client_interface = ClientInterface()
         self.manager_interface = ManagerInterface()
-        print('Выберите товар, еслин не знаете его номер нажмите "?"')
-        self.start = input()
-        self.starting()
+        if start == None:
+            print('Выберите товар, еслин не знаете его номер нажмите "?"')
+            start = input()
+            self.starting(start)
+        else:
+            print()
+            self.starting(start)
 
-    def starting(self):
-        if self.start == 'admin':
-            print('Доступные действия:\n'
-                  'Наполнить магазин,\n'
-                  'Посмотреть остатки\n'
-                  'Добавить товар')
+    def starting(self, start=None):
+        if start == 'admin':
+            print('Введите номер команды:\n'
+                  '1. Наполнить магазин,\n'
+                  '2. Посмотреть остатки\n'
+                  '3. Добавить товар\n'
+                  '4. Удалить товар\n'
+                  '5. Выйти из серв. режима')
             comand = input()
-            if comand == 'Наполнить магазин':
+            if comand == '1':
                 self.manager_interface.fill_the_machine()
-            elif comand == 'Посмотреть остатки':
+            elif comand == '2':
                 self.manager_interface.leftover_goods()
-            elif comand == 'Добавить товар':
+            elif comand == '3':
                 self.manager_interface.add_snacks()
+            elif comand == '4':
+                self.manager_interface.delete_snacks()
+            elif comand == '5':
+                VendingMachine().starting()
             else:
                 print('Недоступная команда')
-        elif self.start == '?':
+        elif start == '?':
             self.client_interface.see_the_assortment()
         else:
-            id_snacks = self.start
+            id_snacks = start
             self.client_interface.buy(id_snacks)
 
 
@@ -41,18 +51,33 @@ class ClientInterface:
         self.storage_service = StorageService()
 
     def see_the_assortment(self):
-        session = self.storage_service.create_session()
-        for item in session.query(ListOfSnacks.id, ListOfSnacks.title, ListOfSnacks.price):
-            print(item.id, item.title, item.price)
+        if os.path.exists('list_of_snacks.db'):
+            session = self.storage_service.create_session()
+            for item in session.query(ListOfSnacks.id, ListOfSnacks.title, ListOfSnacks.price):
+                print(item.id, item.title, item.price)
 
-        print('\n\nВведите номер товара, что бы его купить или "Выход", что бы закончить сеанс')
-        self.buy()
+            print('\n\nВведите номер товара, что бы его купить или "Выход", что бы закончить сеанс')
+            self.buy()
+        else:
+            print('Магазин пуст, обратитесь к менеджеру')
+            VendingMachine().starting()
 
     def buy(self, id_snacks=None):
-        if id_snacks == None:
-            id_snacks = input()
-            if id_snacks == 'Выход':
-                VendingMachine().starting()
+        if os.path.exists('list_of_snacks.db'):
+            if id_snacks == None:
+                id_snacks = input()
+                if id_snacks == 'Выход':
+                    VendingMachine().starting()
+                elif id_snacks == 'admin':
+                    VendingMachine('admin').starting()
+                else:
+                    session = self.storage_service.create_session()
+                    q = session.query(ListOfSnacks).get(id_snacks)
+                    q.quantity -= 1
+                    session.commit()
+                    session.close()
+                    print('Спасибо за покупку!')
+                    VendingMachine().starting()
             else:
                 session = self.storage_service.create_session()
                 q = session.query(ListOfSnacks).get(id_snacks)
@@ -62,27 +87,45 @@ class ClientInterface:
                 print('Спасибо за покупку!')
                 VendingMachine().starting()
         else:
-            if id_snacks == 'Выход':
-                VendingMachine().starting()
-            else:
-                session = self.storage_service.create_session()
-                q = session.query(ListOfSnacks).get(id_snacks)
-                q.quantity -= 1
-                session.commit()
-                session.close()
-                print('Спасибо за покупку!')
-                VendingMachine().starting()
-
+            print('Магазин пуст, обратитесь к менеджеру')
+            VendingMachine().starting()
 
 
 class ManagerInterface:
     # Класс описывает интерфейс взаимодействия менеджера с автоматом
+    def __init__(self):
+        self.storage_service = StorageService()
+
     def fill_the_machine(self):
         pars = Parser()
         pars.parse()
+        print('Магазин наполнен!')
+        VendingMachine('admin').starting()
 
     def leftover_goods(self):
-        pass
+        session = self.storage_service.create_session()
+        for item in session.query(ListOfSnacks.id, ListOfSnacks.title, ListOfSnacks.quantity):
+            print(item.id, item.title, item.quantity)
+        VendingMachine('admin').starting()
 
     def add_snacks(self):
-        pass
+        print('Введите "Название" "Цену в ₽" "Количество"')
+        session = self.storage_service.create_session()
+        title, price, quantity = map(str, input().split())
+        list_of_snacks = ListOfSnacks(title, price + "₽", quantity)
+        session.add(list_of_snacks)
+        session.commit()
+        session.close()
+        print('Товар добавлен!')
+        VendingMachine('admin').starting()
+
+    def delete_snacks(self):
+        print('Введите ID товара которого хотите удалить:')
+        id_snacks = input()
+        session = self.storage_service.create_session()
+        q = session.query(ListOfSnacks).get(id_snacks)
+        session.delete(q)
+        session.commit()
+        session.close()
+        print('Товар удален!')
+        VendingMachine('admin').starting()
